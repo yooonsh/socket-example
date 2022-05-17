b86b6cf04351a3c099180315aeaf542f161f92c8
윤성희 <yoon_sh@cocen.com>
2022-05-17 18:27:14 +0900

    socket voteCount

--- components/SocketTest.vue
+++ components/SocketTest.vue
@@ -11,6 +11,18 @@
         @keydown.enter="getMessage"
       /><button type="button" @click="getMessage">Send</button>
     </form>
+
+    <div class="voteArea">
+      <div>
+        <p>1번: {{ count }}</p>
+        <button @click="voteCount">1번 투표</button>
+      </div>
+      <div>
+        <p>2번: {{ count2 }}</p>
+        <button @click="voteCount2">2번 투표</button>
+      </div>
+    </div>
+    <button @click="voteClear">투표수 초기화</button>
   </div>
 </template>
 
@@ -21,6 +33,8 @@ export default {
       inputMsg: '',
       msgs: [],
       msg: [],
+      count: 0,
+      count2: 0,
     }
   },
   created() {},
@@ -32,14 +46,41 @@ export default {
     this.socket.on('chat-message', (msg) => {
       this.msgs.push(msg)
     })
+
+    this.socket.on('vote', (count) => {
+      this.count = count
+    })
+    this.socket.on('vote2', (count2) => {
+      this.count2 = count2
+    })
   },
   methods: {
     getMessage() {
       this.socket.emit('chat-message', this.inputMsg)
       this.inputMsg = ''
     },
+    voteCount() {
+      this.socket.emit('vote', 1)
+    },
+    voteCount2() {
+      this.socket.emit('vote2', 1)
+    },
+    voteClear() {
+      this.socket.emit('voteClear', 'CLEAR')
+      this.count = 0
+      this.count2 = 0
+    },
   },
 }
 </script>
 
-<style></style>
+<style>
+.voteArea {
+  display: flex;
+  margin: 0 0 20px 0;
+  text-align: center;
+}
+.voteArea div {
+  margin: 0 10px;
+}
+</style>
